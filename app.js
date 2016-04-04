754368a3bdf354bc21263b44e6911c00ee507085
matsken <matsuoka3@gmail.com>
2016-04-04 23:42:52 +0900

    fix vhost settings

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express'),
 	vhost = require('vhost'),
-	raid = require('./raid/raid.js'),
-	config = require("./config"),
-	host = config.vhost;
+	raid = require('./raid/raid.js');
 
 var app = express();
-	app.use(vhost(host, raid));
+	//app.use(vhost(hostname, raid));
 	app.use(vhost("localhost", raid));
 
 module.exports = app;
